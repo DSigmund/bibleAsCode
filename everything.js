51540d336673c87534ac44eee6684c32633ecb5f
Dominik Sigmund <dominik.sigmund@br.de>
2019-02-21 13:05:54 +0100

    Gen 1, 2

--- everything.js
+++ everything.js
@@ -2,5 +2,11 @@ function heaven () {
 
 }
 function earth () {
-  
+  var surface = 'water'
+  var lighting = false
+  holySpirit(this.surface)
+}
+function holySpirit (where) {
+  this.position = where
+  return this
 }
